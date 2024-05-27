435dcb7d6a1cf0544867a2f9b23303bf6a0a2999
minh nhat <nhat892003@gmail.com>
2024-05-27 10:21:38 +0700

    first api

---
+++ GoodDentist/GoodDentist/Controllers/FirstController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoodDentist.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult hello()
+        {
+            return Ok("Hello");
+        }
+    }
+}
