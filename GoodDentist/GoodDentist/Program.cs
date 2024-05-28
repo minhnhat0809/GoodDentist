359691da8c92bf3dcd83122308b2f30b109e4a36
minh nhat <nhat892003@gmail.com>
2024-05-28 23:55:45 +0700

    hash password and salt

--- GoodDentist/GoodDentist/Program.cs
+++ GoodDentist/GoodDentist/Program.cs
@@ -1,3 +1,6 @@
+using Services;
+using Services.Impl;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//service
+builder.Services.AddScoped<IAccountService, AccountService>();
+
+
+// repo
+
+
 
 var app = builder.Build();
 
---
+++ GoodDentist/Service/Impl/AccountService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Impl
+{
+    public class AccountService : IAccountService
+    {
+        private const int saltSize = 128 / 8;
+        private const int keySize = 256 / 8;
+        private const int iterations = 10000;
+        private static readonly HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA256;
+        public string hashPassword(string password)
+        {
+            var salt = RandomNumberGenerator.GetBytes(saltSize);
+
+            var hashedPassword = Rfc2898DeriveBytes.Pbkdf2(password, salt, iterations, hashAlgorithm, keySize);
+
+            return string.Join(Convert.ToBase64String(salt), Convert.ToBase64String(hashedPassword));
+        }
+
+        public bool verifyPassword(string inputPassword, string hashedPassword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
