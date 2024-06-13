77fe2d9543edc14d116a33de8bc5f9fb3bbf560c
minh nhat <nhat892003@gmail.com>
2024-06-13 11:57:43 +0700

    fix error

--- GoodDentist/Service/MapperConfig.cs
+++ GoodDentist/Service/MapperConfig.cs
@@ -64,16 +64,15 @@ namespace Services
             CreateMap<MedicineUpdateDTO, Medicine>()
                 .ReverseMap();
 
-            CreateMap<RecordTypeDTO,  RecordType>()
+            CreateMap<RecordTypeDTO, RecordType>()
                 .ReverseMap();
 
             CreateMap<RecordTypeCreateDTO, RecordType>()
                 .ReverseMap();
-			CreateMap<CreateServiceDTO, Service>().ReverseMap();
+            CreateMap<CreateServiceDTO, Service>().ReverseMap();
             CreateMap<MedicalRecordDTO, MedicalRecord>().ReverseMap();
             CreateMap<MedicalRecordRequestDTO, MedicalRecord>().ReverseMap();
+            CreateMap<CreateRoomDTO, Room>().ReverseMap();
         }
-			CreateMap<CreateRoomDTO, Room>().ReverseMap();
-		}
     }
 }
