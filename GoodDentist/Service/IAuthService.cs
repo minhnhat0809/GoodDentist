ba212536b6a78f5226e811ae0e1a00a1c611998b
Nguyen Vu Truong Huy <nguyenvutruonghuy112@gmail.com>
2024-06-22 14:05:32 +0700

    get account by email + post account password

--- GoodDentist/Service/IAuthService.cs
+++ GoodDentist/Service/IAuthService.cs
@@ -5,4 +5,7 @@ namespace Services;
 public interface IAuthService
 {
     Task<ResponseLoginDTO> Authenticate(LoginDTO loginDto);
+    Task<ResponseDTO> GetAccountByEmailOrPhone(string emailOrPhone);
+
+    Task<ResponseLoginDTO> ResetPassword(Guid userId, string password);
 }
